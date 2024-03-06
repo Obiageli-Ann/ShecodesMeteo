effcfe0cdd5778786f4d021e3c07e471647dba0f
Obiageli-Ann <spicyann001@gmail.com>
2024-03-06 16:47:54 +0100

    Added functionality using API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
+function tempUpdate(response) {
+let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
+temperatureElement.innerHTML = Math.round (temperature);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "9f0940fbf3ca3obdd306d133a0041aft";
+  let apiUrl =
+    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(tempUpdate)
+  }
+
+
 function handleSearchSubmit(event){
   event.preventDefault();
   let inputForm = document.querySelector("#input-form");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = inputForm.value
+
+  searchCity(inputForm.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener ("submit", handleSearchSubmit);
+
+searchCity("Lome");
